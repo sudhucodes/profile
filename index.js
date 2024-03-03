b3ea16500c69fc8fd7ad07a4ec8759e2080f8de1
sudhucodes <151723372+sudhucodes@users.noreply.github.com>
2024-03-03 16:48:33 +0530

    index.js

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = '../Home/home.html';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var homeBtn = document.getElementById('homeBtn');
+
+    homeBtn.addEventListener('click', function () {
+        // Redirect to the user profile page using an absolute path
+        window.location.href = '../Home/home.html';
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the element with class "accountbtndiv"
+    var accountBtn = document.querySelector('.homebtndiv');
+
+    // Add click event listener to the element
+    accountBtn.addEventListener('click', function(event) {
+        // Prevent the default behavior of the anchor tag
+        event.preventDefault();
+        
+        // Redirect to the specified link
+        window.location.href = 'https://sudhucodes.github.io/88-home/';
+    });
+});
