6bf914dd537256e5517a2580373bba725a0ec128
sudhucodes <151723372+sudhucodes@users.noreply.github.com>
2024-03-03 16:51:17 +0530

    index.js

--- index.js
+++ index.js
@@ -12,16 +12,4 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the element with class "accountbtndiv"
-    var accountBtn = document.querySelector('.accountbtndiv');
 
-    // Add click event listener to the element
-    accountBtn.addEventListener('click', function(event) {
-        // Prevent the default behavior of the anchor tag
-        event.preventDefault();
-        
-        // Redirect to the specified link
-        window.location.href = 'https://sudhucodes.github.io/88-home/';
-    });
-});
